53e5145657aedcbabd01a354a5c0e031f088ae5f
Eser Aygün <eser.aygun@gmail.com>
2013-03-22 00:08:33 +0200

    Reformat code for PEP-8 and convert double quotes to single quotes.

--- alignment/profilealigner.py
+++ alignment/profilealigner.py
@@ -1,43 +1,49 @@
 from alignment.profile import *
 from alignment.sequencealigner import *
 
-# Scoring ----------------------------------------------------------------------
+
+# Scoring ---------------------------------------------------------------------
 
 class SoftScoring(Scoring):
 
-	def __init__(self, scoring):
-		self.scoring = scoring
+    def __init__(self, scoring):
+        self.scoring = scoring
 
-	def __call__(self, firstElement, secondElement):
-		score = 0.0
-		for a, p in firstElement.probabilities().iteritems():
-			for b, q in secondElement.probabilities().iteritems():
-				score += p * q * self.scoring(a, b)
-		return score
+    def __call__(self, firstElement, secondElement):
+        score = 0.0
+        for a, p in firstElement.probabilities().iteritems():
+            for b, q in secondElement.probabilities().iteritems():
+                score += p * q * self.scoring(a, b)
+        return score
 
-# Alignment --------------------------------------------------------------------
+
+# Alignment -------------------------------------------------------------------
 
 class ProfileAlignment(SequenceAlignment):
 
-	def __init__(self, first, second, gap=GAP_CODE, other=None):
-		if isinstance(gap, SoftElement):
-			softGap = gap
-		else:
-			softGap = SoftElement({gap: 1})
-		super(ProfileAlignment, self).__init__(first, second, softGap, other)
+    def __init__(self, first, second, gap=GAP_CODE, other=None):
+        if isinstance(gap, SoftElement):
+            softGap = gap
+        else:
+            softGap = SoftElement({gap: 1})
+        super(ProfileAlignment, self).__init__(first, second, softGap, other)
 
-# Aligner ----------------------------------------------------------------------
+
+# Aligner ---------------------------------------------------------------------
 
 class ProfileAligner(SequenceAligner):
 
-	def emptyAlignment(self, first, second):
-		return ProfileAlignment(Profile(), Profile())
+    def emptyAlignment(self, first, second):
+        return ProfileAlignment(Profile(), Profile())
+
 
 class GlobalProfileAligner(ProfileAligner, GlobalSequenceAligner):
-	pass
+    pass
+
 
 class StrictGlobalProfileAligner(ProfileAligner, StrictGlobalSequenceAligner):
-	pass
+    pass
+
 
 class LocalProfileAligner(ProfileAligner, LocalSequenceAligner):
-	pass
+    pass
