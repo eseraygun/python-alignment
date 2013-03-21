f2312d1546eb3f6de75cc03a2dabb427a2174e17
Eser Aygün <eser.aygun@gmail.com>
2013-03-21 23:54:50 +0200

    Update the sequence alignment example.

--- examples/sequencealignment.py
+++ examples/sequencealignment.py
@@ -1,5 +1,9 @@
-# Create sequences to be aligned.
 from alignment.sequence import Sequence
+from alignment.vocabulary import Vocabulary
+from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
+
+
+# Create sequences to be aligned.
 a = Sequence("what a beautiful day".split())
 b = Sequence("what a disappointingly bad day".split())
 print "Sequence A:", a
@@ -7,7 +11,6 @@ print "Sequence B:", b
 print
 
 # Create a vocabulary and encode the sequences.
-from alignment.vocabulary import Vocabulary
 v = Vocabulary()
 aEncoded = v.encodeSequence(a)
 bEncoded = v.encodeSequence(b)
@@ -16,16 +19,14 @@ print "Encoded B:", bEncoded
 print
 
 # Create a scoring and align the sequences using global aligner.
-from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
 scoring = SimpleScoring(2, -1)
 aligner = GlobalSequenceAligner(scoring, -2)
 score, encodeds = aligner.align(aEncoded, bEncoded, backtrace=True)
 
 # Iterate over optimal alignments and print them.
 for encoded in encodeds:
-	alignment = v.decodeSequenceAlignment(encoded)
-	print alignment
-	print "Alignment score:", alignment.score
-	print "Percent identity:", alignment.percentIdentity()
-	print
-
+    alignment = v.decodeSequenceAlignment(encoded)
+    print alignment
+    print "Alignment score:", alignment.score
+    print "Percent identity:", alignment.percentIdentity()
+    print
