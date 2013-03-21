f97c26f76583b37658ad33c9ae73dee3f57e91a1
Eser Aygün <eser.aygun@gmail.com>
2013-03-21 23:26:03 +0200

    Update setup script.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 from distutils.core import setup
 
 setup(
-	name='alignment',
-	version='1.0.2',
-	description='Python Alignment Library',
-	author='Eser Aygun',
-	author_email='eser.aygun@gmail.com',
-	url='https://github.com/eseraygun/python-alignment',
-	requires=['numpy'],
+    name='alignment',
+    version='1.0.3',
+    author='Eser Aygün',
+    author_email='eser.aygun@gmail.com',
+    packages=['alignment'],
+    url='https://github.com/eseraygun/python-alignment',
+    licence='LICENSE.txt',
+    description='Native Python library for generic sequence alignment.',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
 )
