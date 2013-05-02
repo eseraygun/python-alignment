8a344f39824a48fd82f0e1b0a0f8439345a464c0
Eser Aygün <eser.aygun@gmail.com>
2013-05-02 15:12:30 +0300

    Bump the version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='alignment',
-    version='1.0.5',
+    version='1.0.6',
     author='Eser Aygün',
     author_email='eser.aygun@gmail.com',
     packages=['alignment'],
